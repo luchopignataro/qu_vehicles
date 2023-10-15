52cfde1211933486968ba5e0a01a38e454a1cd20
Luciano Pignataro <lucianopignataro.lp@gmail.com>
2023-10-15 15:51:03 -0300

    first commit with basic header

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TopBar />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TopBar from './components/TopBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TopBar
   }
 }
 </script>
 
 <style>
+body{
+  margin: 0;
+  padding: 0;
+  font-size: 16px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,36 @@
+<template>
+    <header>
+        <img class="logo" alt="Vehicles list logo" src="../assets/logo.png">
+        <p>Vehicles list</p>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'TopBar'
+    }
+
+</script>
+
+<style scoped>
+    header{
+        height: 45px;
+        max-height: 45px;
+        border-bottom: 1px solid grey;
+        display: flex;
+        justify-content: start;
+        align-items: center;
+        box-shadow: 0 2px 5px 0 rgba(0,0,0,.16), 0 2px 5px 0 rgba(0,0,0,.23);
+
+        img.logo{
+            max-width: 30px;
+            padding: 5px 15px;
+        }
+
+        p{
+            font-size: 1em;
+            font-weight: 700;
+            margin: 0;
+        }
+    }
+</style>
